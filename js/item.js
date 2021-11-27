21e07c66d90b7fca564727164b9ec104a1fc128a
minhpho8202 <92845747+minhpho8202@users.noreply.github.com>
2021-11-27 18:30:29 +0700

    update

--- js/item.js
+++ js/item.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
       $(".thumbimg").removeClass("active")
       $(this).addClass("active")
     })
-  })
 
+
+  })
   function init() {
     var images = document.querySelectorAll("div.thumb img")
     for (var i = 0; i < images.length;i++)
@@ -14,3 +15,37 @@ $(document).ready(function(){
           img.src = path
         }
   }
+
+  function sizeCheck() {
+    var a = document.getElementById("chieuCao").value
+    var b = document.getElementById("canNang").value
+    a = parseFloat(a)
+    b = parseFloat(b)
+
+    if (a < 165 && b < 55){
+      alert("Size 0")
+    }
+    else {
+      if (a >= 165 && b < 55){
+        alert("Size 1")
+      }
+      else {
+        if (a >= 165 && a < 172 && b < 65){
+          alert("Size 2")
+        }
+        else {
+          if (a >= 170 && a < 177 && b < 80){
+            alert("Size 3")
+          }
+          else {
+            if (a > 172 && b < 100){
+              alert("Size 4")
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+        
