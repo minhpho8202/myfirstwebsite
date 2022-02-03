ff5b7c06e794e611bbc2d4f7e52565d6f09cf784
minhpho8202 <92845747+minhpho8202@users.noreply.github.com>
2022-02-04 00:24:50 +0700

    update

--- js/clothes.js
+++ js/clothes.js
@@ -47,18 +47,29 @@ $(document).ready(function() {
 
     $("div.cart-items").on("click", "div.cart-item > div > input[type=button]", function(){
         $(this).parent().parent().remove()
+        total = 0;
+        count--;
+        updatePrice();
+        if (count == 0){
+            document.getElementsByClassName('price')[0].innerText = '0đ';
+        }
     })
-
+      
 })
+var count = 0;
+var total = 0;
 function init(){
     var popIn = function(){
         const btnAdd = document.querySelectorAll('.btnAdd');
         for(let i = 0;i < btnAdd.length;i++){
             btnAdd[i].addEventListener('click',function(){
                 pushIt(i);
+                total = 0;
+                updatePrice();
+                count++;
             });
         }
-        var total = 0;
+        
         function pushIt(i){
             var item = document.querySelectorAll('div.item')[i];
             var img = item.querySelector('a > img').getAttribute('src');
@@ -80,18 +91,20 @@ function init(){
                     <p>Price:</p>
                     <span>${price}</span>
                 </div>
-                <div class = "col-20 removeBtn">
-                    <input type="button" value="Delete">
+                <div class = "col-20">
+                    <input type="button" value="Delete" class = "removeBtn">
                 </div>
                 `;
                 return itemWrap;
             }(img, name, price);
             document.querySelector('.cart-items').prepend(cartItem);
-            var price2 = parseInt(price.replace('đ', ''))
-            total += price2;
-            document.getElementsByClassName('price')[0].innerText = total + '.000đ';
         }
     }();
+
+    
+
+    
+
     var openBtn = document.querySelector('.open')
     var closeBtn = document.querySelector('.close')
     var modal = document.querySelector('.modal')
@@ -102,5 +115,14 @@ function init(){
     openBtn.addEventListener('click', toggleModal)
     closeBtn.addEventListener('click', toggleModal)
 }
-
+var updatePrice = function(){
+    var items = document.querySelectorAll('div.cart-item > div.pr > span')
+    for (var i = 0;i < items.length;i++)
+    {
+        var price = items[i].innerText;
+        price = parseInt(price.replace('đ', ''))
+        total += price;
+        document.getElementsByClassName('price')[0].innerText = total + '.000đ';
+    }
+}
 
